8952bea3c6c2842fb4b39c20bce76063e0d4061a
dyno-might <dynomight@tutanota.com>
2023-06-28 17:37:40 -0400

    ims

---
+++ tiny/img/deathvalley/generate.sh
@@ -0,0 +1,4 @@
+convert orig.jpg -resize 2791x900 -quality 80 med.webp
+convert orig.jpg -resize 2791x900 -quality 80 med.jpg
+convert orig.jpg -resize 1395x450 -quality 88 small.webp
+convert orig.jpg -resize 1395x450 -quality 88 small.jpg
